015fe3ab6107ff32cefb8658f488ae354092ec27
Paul <paul.fcb55@gmail.com>
2023-09-16 22:38:50 -0500

    curso bases de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Moduls
+import { CounterModule } from './counter/counter.module';
+
+// Components
 import { AppComponent } from './app.component';
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
+  // Here imports the moduls
   imports: [
-    BrowserModule
+    BrowserModule,
+    CounterModule,  //<-- import module "CounterModule"
+    HeroesModule,  //<-- import module "HeroesModule"
+
   ],
   providers: [],
   bootstrap: [AppComponent]
